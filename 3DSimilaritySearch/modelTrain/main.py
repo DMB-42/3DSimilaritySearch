f54b3162a5da97a783fb8ad5a90a33d3602a3337
MinoGamer2000 <dariomunozbarcelo@gmail.com>
2023-07-05 10:00:21 +0200

    Eliminados datos preprocesados

--- 3DSimilaritySearch/modelTrain/main.py
+++ 3DSimilaritySearch/modelTrain/main.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 
 #Comentamos una función de preprocessing en función de si queremos preprocesar los datos o ya lo hicimos y queremos cargarlos
-#from preprocessing import prepare_datasets
-from preprocessing import load_data
+from preprocessing import prepare_datasets
+#from preprocessing import load_data
 
 from model import build_model
 from train import train_model
@@ -23,10 +23,10 @@ NUM_CLASSES = 10
 BATCH_SIZE = 32
 
 #Preparamos los conjuntos de entrenamiento y de prueba
-#train_dataset, test_dataset, CLASS_MAP = prepare_datasets(NUM_POINTS, BATCH_SIZE)
+train_dataset, test_dataset, CLASS_MAP = prepare_datasets(NUM_POINTS, BATCH_SIZE)
 
 #Preparamos los conjuntos de entrenamiento y de prueba a partir de archivos ya procesados
-train_dataset, test_dataset, CLASS_MAP = load_data(BATCH_SIZE)
+#train_dataset, test_dataset, CLASS_MAP = load_data(BATCH_SIZE)
 
 #Creamos y compilamos el modelo
 model = build_model(NUM_POINTS, NUM_CLASSES)
